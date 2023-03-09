da8480d415c0c3bf931319ab83dc18e9ced39f57
Noémi Veres <79631734+noemiveres@users.noreply.github.com>
2023-03-09 13:58:50 +0200

    Add files via upload

---
+++ PT2022_30423_Veres_Noemi_Assignment_4/Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"bll"},{"l":"bll.filter"},{"l":"dal"},{"l":"gui"},{"l":"model"},{"l":"start"},{"l":"utils"}];updateSearchResults();
